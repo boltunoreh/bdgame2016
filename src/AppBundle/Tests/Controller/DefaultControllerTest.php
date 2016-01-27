87389b085ef947aa8b540e88cbfa7fae1f2f01da
kolyadin <donflash@gmail.com>
2016-01-27 19:39:00 +0300

    db interaction support (not ready) --skip-ci

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Tests\Controller;
 
+use Application\Sonata\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -12,14 +13,65 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $admin = new User();
+        $admin->setEmail("adminl@gmail.com");
+        $admin->setUsername("admin");
+        $admin->setPlainPassword("admin");
+        $admin->setEnabled(true);
+        $admin->setSuperAdmin(true);
+
+        $this->em->persist($admin);
+        $this->em->flush();
+    }
+
+
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+    public function testAdminAuth()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/login');
+
+        $form = $crawler->selectButton('_submit')->form();
+
+        $form['_username'] = 'admin';
+        $form['_password'] = 'admin3';
+
+        $crawler = $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+
+        $client->followRedirect();
+
+        print '<pre>' . print_r($client->getResponse()->getContent(), true) . '</pre>';
+
+
+        die;
+
+//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+//        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
     }
 }
