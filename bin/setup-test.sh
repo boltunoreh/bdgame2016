e83c1b4c37842f6f831826f45cc9bcaf72641c63
Artur Vesker <arturvesker@gmail.com>
2016-02-18 18:39:00 +0300

    fix setup scripts

--- bin/setup-test.sh
+++ bin/setup-test.sh
@@ -2,6 +2,6 @@
 
 php app/console doctrine:schema:drop --force -e test
 php app/console doctrine:schema:update --force -e test
-php app/console doctrine:fixtures:load --no-interactive -e test
+php app/console doctrine:fixtures:load -n -e test
 php app/console fos:user:create admin admin@admin.ru admin --super-admin -e test
 php app/console sonata:media:fix-media-context -e test
