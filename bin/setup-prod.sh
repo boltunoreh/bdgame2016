0d184c2bbe72f2cbd7806b58d237a84f8b483271
Artur Vesker <arturvesker@gmail.com>
2016-03-01 13:38:58 +0300

    add media context fixer in production setup

--- bin/setup-prod.sh
+++ bin/setup-prod.sh
@@ -2,6 +2,7 @@
 
 php app/console doctrine:migrations:migrate -n -e prod
 php app/console assets:install -e prod
+php app/console sonata:media:fix-media-context -e prod
 PASSWORD=`date +%s|sha256sum|base64|head -c 32`
 php app/console fos:user:create admin admin@admin.ru $PASSWORD --super-admin
 echo "Admin password: " $PASSWORD;
