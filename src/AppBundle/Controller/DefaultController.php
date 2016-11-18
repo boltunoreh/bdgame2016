1d4601c0f8e4014c0cc7353c5fdea821979037a4
boltun_oreh <alazankin@gmail.com>
2016-11-19 00:31:02 +0300

    controller, temlates some

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,12 +18,24 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      * @Method("GET")
+     * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return;
+    }
+
+    /**
+     * @Route("/fignya", name="fignya")
+     * @Method("GET")
+     * @Template()
+     */
+    public function fignyaAction(Request $request)
+    {
+        $tours = $this->getDoctrine()->getRepository('AppBundle:Tour');
+
+        return array(
+            'tours' => $tours,
+        );
     }
 }
